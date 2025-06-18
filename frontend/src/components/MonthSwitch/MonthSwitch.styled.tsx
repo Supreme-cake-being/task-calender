9cb4e1087672ec719fab6a0a7a7f08f31da5929f
Karr4 but from another universe <XGon20987@gmail.com>
2025-06-18 16:28:34 +0300

    finished month switch

--- frontend/src/components/MonthSwitch/MonthSwitch.styled.tsx
+++ frontend/src/components/MonthSwitch/MonthSwitch.styled.tsx
@@ -4,3 +4,19 @@ export const Box = styled.div`
   display: flex;
   gap: 16px;
 `;
+
+export const Button = styled.button`
+  padding: 8px 16px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background-color: #ccc;
+
+  transition: background-color 250ms linear;
+
+  &:hover {
+    background-color: #aaa;
+  }
+`;
